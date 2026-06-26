e2f7be06c617aa2db2d8cbfaee494067df869c9e
Nathaniel Gebeyaw <nagebeyaw@gmail.com>
2026-06-26 06:01:06 +0000

    Feature: Added SQLITE DB

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import database
 from api_manager import get_total_portfolio
 def calculate_goal_progress(current_equity, goal_amount):
     if goal_amount <= 0:
@@ -9,6 +10,10 @@ def display_portfolio(portfolio_data):
   print("="*40)
     
   total_value = 0.0
+  stock_value = 0
+  crypto_value = 0.0
+
+  
 
   for item in portfolio_data:
     asset = item.get('asset', 'UNKNOWN')
@@ -17,19 +22,24 @@ def display_portfolio(portfolio_data):
     a_type = item.get('asset_type', 'unknown').upper()
         
     total_value += val
+    if a_type == 'STOCK':
+      stock_value += val
+    elif a_type == 'CRYPTO':
+      crypto_value += val
     print(f"[{a_type}] {asset:<8} | QTY: {qty:<10.4f} | VAL: ${val:,.2f}")
         
   print("-" * 40)
   print(f"TOTAL NET WORTH: ${total_value:,.2f}")
   print("="*40)
     
-  return total_value
+  return total_value, stock_value, crypto_value
 if __name__ == "__main__":
+  database.init_db()
   print("Fetching live data from Alpaca & Coinbase...")
   try:
     portfolio_data = get_total_portfolio()
         
-    total_equity = display_portfolio(portfolio_data)
+    total_equity, stock_total, crypto_total = display_portfolio(portfolio_data)
         
     print("\n--- GOAL TRACKER ---")
     user_goal = input("Enter the name of your financial goal (e.g., tuition, laptop, vacation): ")
@@ -52,6 +62,6 @@ if __name__ == "__main__":
       print("-"*40 + "\n")
     else:
       print("\nInvalid amount entered. Please run again and enter a valid number.")
-
+    database.save_snapshot(grand_total, stock_total, crypto_total)
   except Exception as e:
     print(f"\nAn error occurred while fetching data: {e}")
